d3e5309e70adb1b96f5d775df61aa13428cc232c
Bhavin Patil <bhavinpatil56@gmail.com>
2025-08-26 09:53:18 +0000

    Added new dbus service & renamed VirtualSwitch dbus service folder

--- dbus/basic_hello/dbus_hello_service.cpp
+++ dbus/basic_hello/dbus_hello_service.cpp
@@ -4,7 +4,6 @@
 #include<unistd.h> 
 #include<cstring> // for c string functions
 
-
 void reply_to_method_call(DBusMessage* msg, DBusConnection* conn){
     DBusMessage* reply;
     DBusMessageIter args;
---
+++ xyz.openbmc_project.Bhavin.Demo/main.cpp
@@ -0,0 +1,9 @@
+// main.cpp
+
+// Minimal manual DBus service using low-level libdbus (C API) wrapped in C++
+
+// Service: xyz.openbmc_project.Bhavin.Demo 
+// Object Path: /xyz/openbmc_project/bhavin/demo 
+// Interface: xyz.openbmc_project.Bhavin.Demo.Interface
+
+// Build: see Makefile
