2a03d847be8bebbad4054de56c639de176e6448a
Bhavin Patil <bhavinpatil56@gmail.com>
2025-09-18 06:52:50 +0000

    Add sample code for OpenBMC contribution learning journey

---
+++ cplusplus/meta-bhavin-sample-code/taskexample.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<taskexample.h>
+
+int main(){
+    #ifdef WELCOME
+        #if WELCOME == y
+            printf("Welcome to OpenBMC Contrinution Learning journey!\n");
+        #endif
+    #endif
+
+    #ifdef HELLO
+        #if HELLO == y
+            printf("Hello World! from Original Source code for HELLO!\n");
+        #endif
+    #endif
+
+    #ifdef BYE
+        #if BYE == y
+            printf("Bye, keep learning and practicing!\n");
+        #endif
+    #endif
+}
